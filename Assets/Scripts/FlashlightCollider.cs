164f34f2db2cde47955ff4524bd57776c019ff60
allenyuchen <allenyuchen@hotmail.com>
2024-10-12 15:44:26 -0700

    feat: Add one more ray

--- Assets/Scripts/FlashlightCollider.cs
+++ Assets/Scripts/FlashlightCollider.cs
@@ -12,17 +12,21 @@ public class FlashlightCollider : MonoBehaviour
         Ray forward_10_ray = new Ray(flashlight.transform.position, Quaternion.Euler(0, 10, 0) * flashlight.transform.forward);
         Ray forward_20_ray = new Ray(flashlight.transform.position, Quaternion.Euler(0, 20, 0) * flashlight.transform.forward);
         Ray forward_30_ray = new Ray(flashlight.transform.position, Quaternion.Euler(0, 30, 0) * flashlight.transform.forward);
+        Ray forward_40_ray = new Ray(flashlight.transform.position, Quaternion.Euler(0, 40, 0) * flashlight.transform.forward);
         // Increment by 10 degrees counter-clockwise
         Ray forward_neg_10_ray = new Ray(flashlight.transform.position, Quaternion.Euler(0, -10, 0) * flashlight.transform.forward);
         Ray forward_neg_20_ray = new Ray(flashlight.transform.position, Quaternion.Euler(0, -20, 0) * flashlight.transform.forward);
         Ray forward_neg_30_ray = new Ray(flashlight.transform.position, Quaternion.Euler(0, -30, 0) * flashlight.transform.forward);
+        Ray forward_neg_40_ray = new Ray(flashlight.transform.position, Quaternion.Euler(0, -40, 0) * flashlight.transform.forward);
         RaycastHit[] forward_hits;
         RaycastHit[] forward_10_hits;
         RaycastHit[] forward_20_hits;
         RaycastHit[] forward_30_hits;
+        RaycastHit[] forward_40_hits;
         RaycastHit[] forward_neg_10_hits;
         RaycastHit[] forward_neg_20_hits;
         RaycastHit[] forward_neg_30_hits;
+        RaycastHit[] forward_neg_40_hits;
 
         if (flashlight.enabled)
         {
@@ -30,9 +34,11 @@ public class FlashlightCollider : MonoBehaviour
             Debug.DrawRay(forward_10_ray.origin, forward_10_ray.direction * flashlight.range, Color.red);
             Debug.DrawRay(forward_20_ray.origin, forward_20_ray.direction * flashlight.range, Color.red);
             Debug.DrawRay(forward_30_ray.origin, forward_30_ray.direction * flashlight.range, Color.red);
+            Debug.DrawRay(forward_40_ray.origin, forward_40_ray.direction * flashlight.range, Color.red);
             Debug.DrawRay(forward_neg_10_ray.origin, forward_neg_10_ray.direction * flashlight.range, Color.red);
             Debug.DrawRay(forward_neg_20_ray.origin, forward_neg_20_ray.direction * flashlight.range, Color.red);
             Debug.DrawRay(forward_neg_30_ray.origin, forward_neg_30_ray.direction * flashlight.range, Color.red);
+            Debug.DrawRay(forward_neg_40_ray.origin, forward_neg_40_ray.direction * flashlight.range, Color.red);
             forward_hits = Physics.RaycastAll(forward_ray, flashlight.range);
             foreach (RaycastHit hit in forward_hits)
             {
@@ -65,6 +71,14 @@ public class FlashlightCollider : MonoBehaviour
                     return true;
                 }
             }
+            forward_40_hits = Physics.RaycastAll(forward_40_ray, flashlight.range);
+            foreach (RaycastHit hit in forward_40_hits)
+            {
+                if (hit.collider.gameObject == other)
+                {
+                    return true;
+                }
+            }
             forward_neg_10_hits = Physics.RaycastAll(forward_neg_10_ray, flashlight.range);
             foreach (RaycastHit hit in forward_neg_10_hits)
             {
@@ -83,6 +97,14 @@ public class FlashlightCollider : MonoBehaviour
             }
             forward_neg_30_hits = Physics.RaycastAll(forward_neg_30_ray, flashlight.range);
             foreach (RaycastHit hit in forward_neg_30_hits)
+            {
+                if (hit.collider.gameObject == other)
+                {
+                    return true;
+                }
+            }
+            forward_neg_40_hits = Physics.RaycastAll(forward_neg_40_ray, flashlight.range);
+            foreach (RaycastHit hit in forward_neg_40_hits)
             {
                 if (hit.collider.gameObject == other)
                 {
